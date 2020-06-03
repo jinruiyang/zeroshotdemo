71fcd4168bc291a202f0b045bd80f6108040e965
jinruiyang <jinrui.yang@hotmail.com>
2020-06-03 14:39:47 -0700

    inital html

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('ZeroShot.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
@@ -15,10 +15,14 @@ def predict():
     text = list(request.form.values())[0]
     label1 = list(request.form.values())[1]
     label2 = list(request.form.values())[2]
+    label3 = list(request.form.values())[3]
+
     prob1 = compute_prob(text, label1)
     prob2 = compute_prob(text, label2)
+    prob3 = compute_prob(text, label3)
 
-    return render_template('index.html', prediction_text='Prob1 should be {:0.4f}, Prob2 should be {:0.4f}'.format(prob1, prob2))
+
+    return render_template('ZeroShot.html', prediction_text='Prob1: {:0.4f} \n Prob2: {:0.4f} \n Prob3: {:0.4f}'.format(prob1, prob2, prob3))
 
 # @app.route('/results',methods=['POST'])
 # def results():
--- request.py
+++ request.py
@@ -1,6 +1,6 @@
 import requests
 
 url = 'http://localhost:5000/results'
-r = requests.post(url,json={'text':5, 'label1':200})
+r = requests.post(url,json={'text':5, 'label1':200, 'label2':200, 'label3':200, 'label4':200, 'label5':200})
 
 print(r.json())
