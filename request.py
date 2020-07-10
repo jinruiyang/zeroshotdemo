94dd3fd513149edc259724822a8593ed14731bf6
jinruiyang <jinrui.yang@hotmail.com>
2020-07-10 12:40:44 -0700

    remove file

--- request.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = 'http://localhost:5000/results'
-r = requests.post(url,json={'text':5, 'label1':200, 'label2':200, 'label3':200, 'label4':200, 'label5':200})
-
-print(r.json())
